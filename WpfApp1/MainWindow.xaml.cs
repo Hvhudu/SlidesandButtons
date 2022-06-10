d3b063326ea11c5a4cbd67432421556428eabf17
Hvhudu <shtormbreker2@gmail.com>
2022-06-10 13:21:33 +0300

    Done

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Red_but.Background = Red_but.Foreground;
+        }
+
+        private void Blue_but_Click(object sender, RoutedEventArgs e)
+        {
+            Blue_but.Background = Blue_but.Foreground;
+        }
+
+        private void Navy_but_Click(object sender, RoutedEventArgs e)
+        {
+            Navy_but.Background = Navy_but.Foreground;
+        }
+
+        private void Aqua_but_Click(object sender, RoutedEventArgs e)
+        {
+            Aqua_but.Background = Aqua_but.Foreground;
+        }
+
+        private void Teal_but_Click(object sender, RoutedEventArgs e)
+        {
+            Teal_but.Background = Teal_but.Foreground;
+        }
+
+        private void Green_but_Click(object sender, RoutedEventArgs e)
+        {
+            Green_but.Background = Green_but.Foreground;
+        }
+
+        private void Olive_but_Click(object sender, RoutedEventArgs e)
+        {
+            Olive_but.Background = Olive_but.Foreground;
+        }
+
+        private void Lime_but_Click(object sender, RoutedEventArgs e)
+        {
+            Lime_but.Background = Lime_but.Foreground;
+        }
+
+        private void Yellow_but_Click(object sender, RoutedEventArgs e)
+        {
+            Yellow_but.Background = Yellow_but.Foreground;
+        }
+
+        private void Orange_but_Click(object sender, RoutedEventArgs e)
+        {
+            Orange_but.Background = Orange_but.Foreground;
+        }
+
+        private void Maroon_but_Click(object sender, RoutedEventArgs e)
+        {
+            Maroon_but.Background = Maroon_but.Foreground;
+        }
+
+        private void Fuchsia_but_Click(object sender, RoutedEventArgs e)
+        {
+            Fuchsia_but.Background = Fuchsia_but.Foreground;
+        }
+
+        private void Purple_but_Click(object sender, RoutedEventArgs e)
+        {
+            Purple_but.Background = Purple_but.Foreground;
+        }
+
+        private void Black_but_Click(object sender, RoutedEventArgs e)
+        {
+            Black_but.Background = Black_but.Foreground;
+        }
+
+        private void Silver_but_Click(object sender, RoutedEventArgs e)
+        {
+            Silver_but.Background = Silver_but.Foreground;
+        }
+
+        private void Gray_but_Click(object sender, RoutedEventArgs e)
+        {
+            Gray_but.Background = Gray_but.Foreground;
+        }
+
+        private void White_but_Click(object sender, RoutedEventArgs e)
+        {
+            White_but.Background = White_but.Foreground;
+        }
+    }
+}
