0a128d829542534924b6a306869f0f68ffeb2127
Hvhudu <shtormbreker2@gmail.com>
2022-06-10 12:11:47 +0300

    Add project files.

---
+++ Slides/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Slides
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.prev = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 380);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(85, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(12, 415);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(85, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // prev
+            // 
+            this.prev.Location = new System.Drawing.Point(135, 415);
+            this.prev.Name = "prev";
+            this.prev.Size = new System.Drawing.Size(85, 23);
+            this.prev.TabIndex = 2;
+            this.prev.Text = "Prev";
+            this.prev.UseVisualStyleBackColor = true;
+            this.prev.Click += new System.EventHandler(this.prev_Click);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(247, 415);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(85, 23);
+            this.open.TabIndex = 3;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(358, 415);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(85, 23);
+            this.next.TabIndex = 4;
+            this.next.Text = "Next";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 351);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(524, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 49);
+            this.label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.prev);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button prev;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Slides/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Slides
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        List<Bitmap> images = new List<Bitmap>();
+        int count = 0;
+        Bitmap one;
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 500;
+            timer.Tick += next_Click;
+        }
+        private void open_Click(object sender, EventArgs e)
+        {
+            stop_Click(null, null);
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                timer.Stop();
+                if (images.Count != 0) 
+                {
+                    foreach (var item in images)
+                    {
+                        item.Dispose();
+                    }
+                    images.Clear();
+                }
+
+                DirectoryInfo info = new DirectoryInfo(folder.SelectedPath);
+                IEnumerable<FileInfo> files = info.EnumerateFiles();
+                foreach(var item in files)
+                {
+                    string ex = Path.GetExtension(item.FullName);
+                    if (ex == ".bmp" || ex == ".jpg" || ex == ".jpeg" || ex == ".png")
+                    {
+                        Bitmap pt = new Bitmap(item.FullName);
+                        Size pt_size = pictureBox1.Size;
+                        images.Add(new Bitmap(pt, pictureBox1.Size));
+                    }
+                   
+                }
+            }
+            count = 0;
+            label1.Text = " ";
+            pictureBox1.Image = images[count];
+            label1.Text = Convert.ToString((count + 1) + "/" + images.Count);
+        }
+
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0)
+            {
+                return;
+            }
+            count++;
+            if (count >= images.Count)
+            {
+                count = 0;
+            }
+            pictureBox1.Image = images[count];
+            label1.Text = Convert.ToString((count + 1) + "/" + images.Count);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void prev_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0)
+            {
+                return;
+            }
+            count--;
+            if (count >= images.Count)
+            {
+                count = 0;
+            }
+            pictureBox1.Image = images[count];
+            label1.Text = Convert.ToString((count + 1) + "/" + images.Count);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (images.Count != 0)
+            {
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Папка не выбрана");
+            }
+        }
+    }
+}
